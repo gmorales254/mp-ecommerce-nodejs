13111e6be7ecfe6db2e14b6b44ab895478b634f7
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2023-02-27 08:09:53 -0300

    Desarrollo de api "payment"
    - Ahora se puede realizar una request a la api de MercadoPago con la info de un solo producto de momento.

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+const mercadopago = require('mercadopago')
+require('dotenv').config()
 var port = process.env.PORT || 3000
 
+
 var app = express();
- 
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.use(express.static('assets'));
- 
+
 app.use('/assets', express.static(__dirname + '/assets'));
+
+app.use((req, res, next) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    next();
+})
+
+// configure app to use body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 
 app.get('/', function (req, res) {
     res.render('home');
@@ -19,4 +35,36 @@ app.get('/detail', function (req, res) {
     res.render('detail', req.query);
 });
 
+
+// integracion con mercado pago
+mercadopago.configure({ access_token: process.env.MERCADOPAGO_KEY });
+
+app.post('/payment', function (req, res) {
+    let prod = req.body;
+
+    let preference = {
+        items: [
+            {
+                title: prod.title,
+                unit_price: prod.price,
+                quantity: prod.quantity,
+            }
+        ],
+        back_urls: {
+            success: "http://localhost:3000",
+            failure: "",
+            pending: ""
+        },
+        auto_return: "approved",
+        binary_mode: true
+    };
+
+    mercadopago.preferences.create(preference)
+        .then((response) => {
+            res.status(200).send({ response });
+        }).catch((error) => {
+            res.status(400).send({ error: error.message })
+        });
+
+})
 app.listen(port);
